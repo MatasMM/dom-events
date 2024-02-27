44c0e618168a7d7546a3dd20d341781555df99bb
MatasMM <153711311+MatasMM@users.noreply.github.com>
2024-02-27 14:21:36 +0200

    test

--- js/header.js
+++ js/header.js
@@ -1,6 +1,8 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
-    const href = location.hostname === 'localhost' ? '/' : '/';
+    const href = location.hostname === 'localhost' ? '/' : './';
+
+    console.log(href);
 
     const HTML = `
         <a href="${href}">Home</a>
